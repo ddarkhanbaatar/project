94dc7ec6ea4b530e741f10744afa03cae1bcad30
ddarkhanbaatar <darkhanbaatar@april9.com.au>
2019-10-23 19:00:37 +1000

    Remove output messages

--- src/main/java/infs7410/project1/part3/BM25_RSJ.java
+++ src/main/java/infs7410/project1/part3/BM25_RSJ.java
@@ -29,8 +29,8 @@ public class BM25_RSJ {
         double RJS_weight = Math.log(weight1 / weight2);
         double saturation = ((this.k1 + 1.0D) * docFeq / (k1 * B + docFeq));
         double within_query = ((this.k2 + 1.0D) * queryFeq / (this.k2 + queryFeq));
-        System.out.println("N:" + N + ", R:" + R + ", queryReq:" + queryFeq + ", docFeq:" + docFeq + ", docLen:" + docLen + ", avgDocLen:" + avgDocLen);
-        System.out.println(String.format("w1:%f, w2:%f, sat:%f, query_within:%f", weight1, weight2, saturation, within_query));
+        //System.out.println("N:" + N + ", R:" + R + ", queryReq:" + queryFeq + ", docFeq:" + docFeq + ", docLen:" + docLen + ", avgDocLen:" + avgDocLen);
+        //System.out.println(String.format("w1:%f, w2:%f, sat:%f, query_within:%f", weight1, weight2, saturation, within_query));
 
         return RJS_weight * saturation * within_query;
     }
@@ -51,7 +51,7 @@ public class BM25_RSJ {
             int docFeq = docInfo.getDocumentFrequency();
             int docLen = docInfo.getDocumentLength();
             double score = score(queryFeq, docFeq, docLen, avgDocLen, N, termRelevance.get(queryTerm).getNi(), R, termRelevance.get(queryTerm).getRi());
-            System.out.println("Term:" + queryTerm + ", Score:" + score);
+            // System.out.println("Term:" + queryTerm + ", Score:" + score);
             totalScore += score;
         }
 
